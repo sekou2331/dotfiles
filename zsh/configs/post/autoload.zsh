068ad1fcc07a88051e9135c17de2f7237ca89b9c
Amal Sudama <cds.sudama@gmail.com>
2018-01-26 09:09:59 -0500

    enable zmv from zshcontrib

---
+++ zsh/configs/post/autoload.zsh
@@ -0,0 +1,6 @@
+# The zsh contains a very usefull extension called zmv. You can load it via
+# autoload -U zmv. The man page is located under zshcontrib. A very usefull flag
+# is -n which does not execute the command but prints the command that would be
+# executed. Use this if you are unsure about your patterns
+
+autoload -U zmv
